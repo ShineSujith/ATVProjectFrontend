bae87ecdeb184be88bc02bba0b4c627d4038e9a2
ShineSujith <118687029+ShineSujith@users.noreply.github.com>
2026-02-17 15:15:12 +0000

    added websocket code

--- components/videoPlayer/VideoPlayer.js
+++ components/videoPlayer/VideoPlayer.js
@@ -1,13 +1,60 @@
-import classes from "./VideoPlayer.module.css"
+import classes from "./VideoPlayer.module.css";
+import { useState, useRef, useEffect } from "react";
 
 function VideoPlayer() {
-    return(
-        <div className={classes.container}>
-            <video width="740" height="500" controls>
-                <source src="test.mp4" type="video/mp4"/> {/*TODO: replace this test video with websocket sent video*/}
-            </video>
-        </div> 
-    );
+  const videoRef = useRef(null);
+  const [queue, setQueue] = useState([]);
+  const [current, setCurrent] = useState(null);
+  const ws = useRef(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8000/ws");
+
+    ws.current.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (data.type === "video_ready") {
+        const url = `http://localhost:8000/videos/${data.filename}`;
+        console.log(data.filename);
+
+        setQueue((q) => [...q, url]);
+      }
+    };
+
+    return () => ws.current.close();
+  }, []);
+
+  // Start playing when idle
+  useEffect(() => {
+    if (!current && queue.length > 0) {
+      setCurrent(queue[0]);
+      setQueue((q) => q.slice(1));
+    }
+  }, [queue, current]);
+
+  const handleEnded = () => {
+    if (queue.length > 0) {
+      setCurrent(queue[0]);
+      setQueue((q) => q.slice(1));
+    } else {
+      setCurrent(null);
+    }
+  };
+
+  return (
+    <div className={classes.container}>
+      <video
+        width="740"
+        height="500"
+        preload="auto"
+        controls
+        ref={videoRef}
+        src={current}
+        autoPlay
+        onEnded={handleEnded}
+      />
+    </div>
+  );
 }
 
 export default VideoPlayer;
